ecce291077b219765de4322bdb8fb06f1120bc59
Santiago Flores Kanter <sfloresk@cisco.com>
2019-05-28 09:00:13 -0600

    add docker commands

--- start_vms.sh
+++ start_vms.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+docker rm -f devnet_3000
+docker run -d -p 12345:8080 --name devnet_3000 sfloresk/devnet_3000
 curl -O https://dl.google.com/go/go1.12.3.linux-amd64.tar.gz
 vagrant up
